98ce99d8278803aec8ec8102fd356a1bbbdd448f
JesseVictors <jvictors@jessevictors.com>
2013-01-25 13:18:52 -0700

    now creates 2D randomized mountain, but has tearing

--- Sierpinski.cpp
+++ Sierpinski.cpp
@@ -1,24 +1,26 @@
+
 #include <vector>
 #include <map>
 #include <random> //for Mersenne Twister RNG
-#include <cmath> //for sqrt, pow, and abs
-#include <algorithm> //for transform
+#include <cmath>
+#include <math.h> //for calculating distance between two Points
+#include <algorithm> //for iteration
 
 #include "Triangle.struct"
 
-const int SCALE = 0.1;
+const float SCALE = 0.1f;
+
+std::mt19937 mersenneTwister; //Mersenne Twister PRNG. WAY better randomness!
+std::uniform_real_distribution<float> randomFloat(0, 1);
 
 
 /* Produces a vector with the coordinates randomized and in the range of (0, 1) */
 Point randVector()
 {
-	std::mt19937 generator; //Mersenne Twister PRNG. WAY better randomness!
-	std::uniform_real_distribution<float> distribution(0, 1);
-
-	Point pt;
-	pt.x = distribution(generator);
-	pt.y = distribution(generator);
-	return pt;
+	return { //return random coordinates, each in the range of (0, 1)
+		randomFloat(mersenneTwister),
+		randomFloat(mersenneTwister)
+	};
 }
 
 
@@ -26,7 +28,7 @@ Point randVector()
 /* Returns the distance between the two given Points */
 float length(const Point& a, const Point& b)
 {
-	return sqrt(pow(abs(a.x - b.x), 2) + pow(abs(a.y - b.y), 2)); //sqrt of a^2 + b^2
+	return sqrt(pow(fabs(a.x - b.x), 2) + pow(fabs(a.y - b.y), 2)); //sqrt of a^2 + b^2
 }
 
 
@@ -41,7 +43,7 @@ Point getMidpoint(const Point& a, const Point& b)
 		return foundResult->second; //if found in cache, return pre-computed midpoint
 	
 	auto result = (a + b) / 2; //create midpoint
-	//result += randVector() * (length(a, b) * SCALE); //create random offset
+	result += randVector() * length(a, b) * SCALE; //create random offset
 
 	memo.insert(std::make_pair(std::make_pair(a, b), result)); //memoize
 	memo.insert(std::make_pair(std::make_pair(b, a), result));
@@ -73,7 +75,7 @@ std::vector<Triangle> subdivideTriangle(const Triangle& triangle)
 		{ triangle.A, cPrime, bPrime }, //top triangle
 		{ cPrime, triangle.B, aPrime }, //bottom left triangle
 		{ bPrime, aPrime, triangle.C }, //bottom right triangle
-		{ cPrime, aPrime, bPrime }      //center triangle
+		{ cPrime, aPrime, bPrime }      //center triangle, comment this out to 
 	};
 }
 
@@ -116,32 +118,35 @@ std::vector<Triangle> getGasket(const int& maxDepth)
 
 
 
+/* Adds the line formed between the two Points to the vector of vertices */
+void appendLine(std::vector<GLfloat>& vertices, const Point& a, const Point& b)
+{
+	vertices.push_back(a.x);
+	vertices.push_back(a.y);
+	//vertices.push_back(a.z);
+
+	vertices.push_back(b.x);
+	vertices.push_back(b.y);
+	//vertices.push_back(a.z);
+}
+
+
+
 /* Returns the vertices that describe the on-screen shapes */
 std::pair<int, std::vector<GLfloat>> getVertices()
 {
 	int coordinateSize = 2;
 
-	auto gasketTriangles = getGasket(4); //8 is a good max
-	std::cout << "Triangle count: " << gasketTriangles.size() << std::endl;
+	auto gasket = getGasket(5); //8 is a good max
+	std::cout << "Triangle count: " << gasket.size() << std::endl;
 
 	std::vector<GLfloat> vertices;
-	for_each (gasketTriangles.begin(), gasketTriangles.end(), 
-		[&](const Triangle& gasketTriangle)
+	for_each (gasket.begin(), gasket.end(), //iterate through all the gasket's triangles
+		[&](const Triangle& triangle)
 		{
-			vertices.push_back(gasketTriangle.A.x);
-			vertices.push_back(gasketTriangle.A.y);
-			vertices.push_back(gasketTriangle.B.x);
-			vertices.push_back(gasketTriangle.B.y);
-
-			vertices.push_back(gasketTriangle.B.x);
-			vertices.push_back(gasketTriangle.B.y);
-			vertices.push_back(gasketTriangle.C.x);
-			vertices.push_back(gasketTriangle.C.y);
-
-			vertices.push_back(gasketTriangle.C.x);
-			vertices.push_back(gasketTriangle.C.y);
-			vertices.push_back(gasketTriangle.A.x);
-			vertices.push_back(gasketTriangle.A.y);
+			appendLine(vertices, triangle.A, triangle.B); //express each triangle as three lines
+			appendLine(vertices, triangle.B, triangle.C);
+			appendLine(vertices, triangle.C, triangle.A);
 
 			//std::cout << "A: " << gasketTriangle.A.x << ", " << gasketTriangle.A.y << std::endl;
 			//std::cout << "B: " <<gasketTriangle.B.x << ", " << gasketTriangle.B.y << std::endl;
